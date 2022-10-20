71e73015a869d2f960b690aac6938081f712d30c
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-20 10:52:45 +0500

    Lug'at 14 dars amaliyot
    Bu yerda 14-darsning amaliyotlari bor

---
+++ lug'at_14-dars_Amaliyot.py
@@ -0,0 +1,75 @@
+##otam = {'ism':'Xolmo\'min',
+##        't_yil':1975,
+##        'shahar':'Nurota'
+##        }
+##onam = {'ism':'Yulduz',
+##        't_yil':1979,
+##        'shahar':'Nurota'
+##        }
+##toga = {'ism':'Asror',
+##        't_yil':1999,
+##        'shahar':'Nurota'}
+##print(f"Otamning ismi {otam['ism']}, {otam['t_yil']}-yilda,\
+## {otam['shahar']} shahrida tug'ilgan")
+##print(f"Onamning ismi {onam['ism']}, {onam['t_yil']}-yilda,\
+## {onam['shahar']} shahrida tug'ilgan")
+##print(f"Tog'amning ismi {toga['ism']}, {toga['t_yil']}-yilda,\
+## {toga['shahar']} shahrida tug'ilgan")
+
+##sevimli_taom = {
+##    'dadam':'honim',
+##    'ayam':'sho\'rva',
+##    'togam':'manti',
+##    'ukam':'lavash',
+##    'singlim':'pishirilgan tovuq go\'sht'
+##    }
+##sevimli_taom['sardorbek'] = 'osh'
+##print(f"Ayamning sevimli taomi {sevimli_taom['ayam']}")
+##print(f"Ukamning sevimli taomi {sevimli_taom['ukam']}")
+##print(f"Singlimning sevimli taomi {sevimli_taom['singlim']}")
+##print(f"Mening sevimli taomim {sevimli_taom['sardorbek']}")
+
+##pyLugat = {
+##    'integer':'butun son',
+##    'float':'haqiqiy son',
+##    'string':'matn',
+##    'list':'ro\'yxat',
+##    'tumple':'o\'zgarmas',
+##    'variable':'o\'zgaruvchi',
+##    'if':'agar',
+##    'for':'uchun',
+##    'else':'akis holda',
+##    'number':'son'
+##    }
+##lugat = pyLugat.get(input("Kalit so'z kiriting: "),"Bunday so'z mavjud emas")
+##print(lugat)
+
+##lugat = input("Kalit so'z kiriting: ")
+##
+##if lugat in pyLugat:
+##    print(f"{lugat.title()} so'zi {pyLugat.get(lugat).title()} deb tarjima qilinadi")
+##else:
+##    print("Bunday so'z mavjud emas")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
