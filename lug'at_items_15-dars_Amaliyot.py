605ed896a77ccc08aba099d664c15edd7d29c95d
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-20 23:44:12 +0500

    lugat bilan ishlash 15 dars amaliyot
    Bu yerda 15-darsning 1, 2, 3 javoblari bor

---
+++ lug'at_items_15-dars_Amaliyot.py
@@ -0,0 +1,81 @@
+##py_lugat = {
+##    'boolean':'mantiqiy qiymat',
+##    'float':'haqiqiy son',
+##    'for':'biror amalni qayta-qayta bajarish',
+##    'integer':'butun son',
+##    'if':'shartni tekshrish',
+##    'string':'matn',
+##    'upper':'berilgan so\'zni hammasini kattalashtirish',
+##    'items':'kalit va qiymatga birdaniga murojaat qilish',
+##    'list':'lug\'at',
+##    'sorted':'kelgan so\'zlarni alifbo tartibida tartiblash'
+##    }
+##for k, q in sorted(py_lugat.items()):
+##    print(f"{k.title()} - {q.capitalize()}")
+
+davlatlar = {
+    'aqsh':'washington d.c',
+    'o\'zbekiston':'toshkent',
+    'tojikiston':'dushanbe',
+    'yaponiya':'tokio',
+    'hindiston':'dehli',
+    'italiya':'rim',
+    'malayziya':'kuala-lumpur',
+    'rossiya':'moskva',
+    'singapur':'singapur',
+    'qozog\'iston':'astana',
+    'qirg\'ziston':'bishkek',
+    'saudiya arabistoni':'ar-riyot'
+    }
+print("Dunyo davlatlari:")
+for davlat in sorted(davlatlar.keys()):
+    print(davlat.upper())
+    
+print("\nDavlatlarning poytaxtlari:")
+for poytaxt in sorted(davlatlar.values()):
+    print(poytaxt.title())
+    
+foy_chi = input("\nQaysi davlatning poytaxtini bilishni istaysiz?: ").lower()
+poy_taxt = davlatlar.get(foy_chi,"Bizda bunday ma'lumot yo'q")
+if foy_chi in davlatlar:
+    print(f"{foy_chi.upper()}ning poytaxti {poy_taxt.title()} shahri")
+##else:
+##    print("Bizda bunday ma'lumot yo'q")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
