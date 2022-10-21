110e8577572ddfc9bc024c4f9bb419c778368ba5
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-21 07:49:11 +0500

    15-dars items(), set() lug'at amaliyot
    Bu yerda 15-darsning amaliyoti bor,
    javoblari

--- lug'at_items_15-dars_Amaliyot.py
+++ lug'at_items_15-dars_Amaliyot.py
@@ -13,48 +13,57 @@
 ##for k, q in sorted(py_lugat.items()):
 ##    print(f"{k.title()} - {q.capitalize()}")
 
-davlatlar = {
-    'aqsh':'washington d.c',
-    'o\'zbekiston':'toshkent',
-    'tojikiston':'dushanbe',
-    'yaponiya':'tokio',
-    'hindiston':'dehli',
-    'italiya':'rim',
-    'malayziya':'kuala-lumpur',
-    'rossiya':'moskva',
-    'singapur':'singapur',
-    'qozog\'iston':'astana',
-    'qirg\'ziston':'bishkek',
-    'saudiya arabistoni':'ar-riyot'
-    }
-print("Dunyo davlatlari:")
-for davlat in sorted(davlatlar.keys()):
-    print(davlat.upper())
-    
-print("\nDavlatlarning poytaxtlari:")
-for poytaxt in sorted(davlatlar.values()):
-    print(poytaxt.title())
-    
-foy_chi = input("\nQaysi davlatning poytaxtini bilishni istaysiz?: ").lower()
-poy_taxt = davlatlar.get(foy_chi,"Bizda bunday ma'lumot yo'q")
-if foy_chi in davlatlar:
-    print(f"{foy_chi.upper()}ning poytaxti {poy_taxt.title()} shahri")
+##davlatlar = {
+##    'aqsh':'washington d.c',
+##    'o\'zbekiston':'toshkent',
+##    'tojikiston':'dushanbe',
+##    'yaponiya':'tokio',
+##    'hindiston':'dehli',
+##    'italiya':'rim',
+##    'malayziya':'kuala-lumpur',
+##    'rossiya':'moskva',
+##    'singapur':'singapur',
+##    'qozog\'iston':'astana',
+##    'qirg\'ziston':'bishkek',
+##    'saudiya arabistoni':'ar-riyot'
+##    }
+##print("Dunyo davlatlari:")
+##for davlat in sorted(davlatlar.keys()):
+##    print(davlat.upper())
+##    
+##print("\nDavlatlarning poytaxtlari:")
+##for poytaxt in sorted(davlatlar.values()):
+##    print(poytaxt.title())
+##    
+##foy_chi = input("\nQaysi davlatning poytaxtini bilishni istaysiz?: ").lower()
+##poy_taxt = davlatlar.get(foy_chi)
+##if foy_chi in davlatlar:
+##    print(f"{foy_chi.upper()}ning poytaxti {poy_taxt.title()} shahri")
 ##else:
 ##    print("Bizda bunday ma'lumot yo'q")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+##menu = {
+##    'osh':'15000',
+##    'manti':'15000',
+##    'norin':'30000',
+##    'chuchvara':'20000',
+##    'somsa':'5000',
+##    'non':'4000',
+##    'dolma':'25000',
+##    'kabob':'7000',
+##    'lag\'mon':'12000',
+##    'bishtekis':'15000',
+##    }
+##buyurtmalar = []
+##print("3 ta taom buyurtma bering.")
+##for taom in range(3):
+##    buyurtmalar.append(input(f"{taom+1}-taom: ").lower())
+##
+##for mahsulot in buyurtmalar:
+##    if mahsulot in menu:
+##        print(f"{mahsulot.title()} {menu[mahsulot]} so'm")
+##    else:
+##        print(f"Bizda {mahsulot} yo'q")
 
 
 
