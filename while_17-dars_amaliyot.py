3288d739ec6d53268aa2313b8f9b5fbbb52e1c00
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-24 15:45:36 +0500

    while 17 dars amaliyoti
     While 17-dars mavzusiga doir amaliyot javoblari

---
+++ while_17-dars_amaliyot.py
@@ -0,0 +1,50 @@
+# 1-javob
+##savol = "Yaxshi ko'rgan kitobinggizni kiriting \n"
+##savol += "(Kiritib bo'lgach 'stop' deb kiriting): "
+##qiymat = ''
+##while qiymat != 'stop':
+##    qiymat = input(savol)
+##    if qiymat == 'stop':
+##        break
+##print("Dastur tugadi")
+
+# 2-javob
+##print("Foydalanuvchi yoshiga qarab chipta narxini aytuvchi dastur.")
+##foyda = "\nFoydalanuvchi yoshingizni kiriting\n"
+##foyda += "Dasturni tugatish uchun 'exit' yoki 'quit' deb yozing "
+##
+##while True:
+##    value = input(foyda)
+##    if value == 'exit' or value == 'quit':
+##        break
+##    elif int(value) < 7:
+##        price = 2000
+##        print(f"\nSizga chipta {price} so'm")
+##    elif int(value) < 18:
+##        price = 3000
+##        print(f"\nSizga chipta {price} so'm")
+##    elif int(value) < 65:
+##        price = 10000
+##        print(f"\nSizga chipta {price} so'm")
+##    elif int(value) > 65:
+##        price = 0
+##        print(f"\nSizga chipta {price} so'm")
+##print("Dastur tugadi")
+
+# 3-javob
+##savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+##savol += "Musbat son kiriting "
+##savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+##
+##while True:
+##    qiymat = input(savol)
+##    if qiymat == 'exit':
+##        break
+##    elif float(qiymat) < 0:
+##        continue
+##    else:
+##        ildiz = float(qiymat)**(0.5)
+##        print(f"{qiymat} ning ildizi {ildiz} ga teng") 
+##print("dastur tugadi")
+
+
