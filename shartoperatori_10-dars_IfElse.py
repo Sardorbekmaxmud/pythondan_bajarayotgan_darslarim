8a9eaaff6574384b9089f4cba24cc75a94805ead
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-24 15:50:49 +0500

    If Else 10-dars
    10-dars amaliyoti javoblari

---
+++ shartoperatori_10-dars_IfElse.py
@@ -0,0 +1,76 @@
+#ism = 'ahad'
+#print(ism!='Ahad')
+#print(ism.title()=='Ahad')
+#nums1 = [1,2,3]
+#nums2 = [1,2,4]
+#print(nums1!=nums2)
+#print(9**2>=7*9+18)
+#x = 10
+#print(x*x>=float(f"{x**2}"))
+
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh >= 18:
+#    print('Xush kelibsiz')
+#else:
+#    print('KIrish mumkin emas')
+
+##yil = int(input("Tug'ilgan yilingizni kiriting?>>> "))
+##if 2022 - yil <= 18:
+##    print(f"Sizni yoshingiz {2022 - yil} da ekan!")
+##    print("Kirish mumkin emas")
+##else:
+##    print("Xush galdingiz")
+
+##cars = ['toyota','mazda','hyundai','gm','kia']
+##for car in cars:
+##    if car != 'gm':
+##        print(car.upper())
+##    else:
+##        print(car.title())
+
+##login = input("Loginni kiriting!>>>")
+##if login.title() == "Admin":
+##    print("Xush kelibsiz, Admin.\nFoydalanuvchilar ro'yxatini ko'rasizmi?")
+##else:
+##    print(f"Xush kelibsiz, {login.title()}!")
+##
+##son1 = int(input("son kiriting!>>>"))
+##son2 = int(input("son kiriting!>>>"))
+##if son1 == son2:
+##    print("Sonlar teng")
+
+##son = int(input("Istalgan son kiriting>>>"))
+##if son < 0:
+##    print("Manfiy son")
+##else:
+##    print("Musbat son")
+
+##son = int(input("Iltimos son kiriting!!!"))
+##if son >= 0:
+##    print(f"{son ** 0.5}")
+##else:
+##    print("Musbat son kiriting")
+
+##son = int(input("Son kiriting>>>"))
+##if son % 2 == 0:
+##    print("Juft")
+##else:
+##    print("Toq")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
