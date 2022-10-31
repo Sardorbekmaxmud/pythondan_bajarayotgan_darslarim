18c9146d6756f1b7aa9ec623ec60ca33149db99e
Sardorbek Maxmudov_X <102671619+Sardorbekmaxmud@users.noreply.github.com>
2022-10-31 20:47:02 +0500

    toplam_mavzu_amaliyot
    To'plam mavzusidagi amaliyot javoblari

---
+++ To'plam mavzusidagi amaliyot/toplam_mavzu_amaliyot.py
@@ -0,0 +1,32 @@
+# 1-2-javob 
+#rang = {'qizil','qora'}
+#rang_2 = {"ko'k","oq"}
+#rang_3 = {"sariq","yashil"}
+#rang.add("pushti") # rang qo'shish boshqa str kiritsa ham bo'ladi
+#rang_2.update(["kulrang","siyohrang"]) # 2 ta va undan ko'p element qo'shish
+#rang_3.add("jigar rang")
+
+# 3-4-5-javob
+#set1 = {10, 20, 30, 40, 50}
+#set2 = {30, 40, 50, 60, 70}
+#set3 = set1&set2 # ikkalasida borlari
+#print(set3)
+#print(set1-set2) # set1 da bor set2 da yo'q
+#print(set2-set1)  # set2 da bor set1 da yo'q
+#print(set1^set2) #simmetrik farq bi-birida yo'qlari
+
+# 6-7-8-9-javob
+##bozorlik = ['choy','non','kartoshka','tuxum','sut']
+##mahsulotlar = ['non','sut','tuxum','olma','un','tuz']
+##bor_mahsulotlar = []
+##mavjud_emas = []
+##for mahsulot in bozorlik:
+##    if mahsulot in mahsulotlar:
+##        bor_mahsulotlar.append(mahsulot)
+##    else:
+##        mavjud_emas.append(mahsulot)
+##print(bor_mahsulotlar)
+##print(mavjud_emas)
+##mahsulotlar.append('kartoshka')
+##mahsulotlar.insert(4,'choy')
+##print(mahsulotlar)
